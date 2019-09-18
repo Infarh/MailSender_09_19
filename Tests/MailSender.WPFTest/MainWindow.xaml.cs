bd60f810179ff1c803145a709bf898faca3b2a58
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-18 20:32:08 +0300

    Тестовое приложение для отправки почты

--- Tests/MailSender.WPFTest/MainWindow.xaml.cs
+++ Tests/MailSender.WPFTest/MainWindow.xaml.cs
@@ -1,28 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Security;
 
 namespace MailSender.WPFTest
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void SendButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var host = "smtp.yandex.ru";
+            var port = 25;
+
+            var user_name = UserNameEditor.Text;
+            var password = PasswordEditor.SecurePassword;
+
+            var msg = "Hello World!!! " + DateTime.Now;
+
+            using (var client = new SmtpClient(host, port))
+            {
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(user_name, password);
+
+                using (var message = new MailMessage())
+                {
+                    message.From = new MailAddress("shmachilin@yandex.ru", "Павел");
+                    message.To.Add(new MailAddress("shmachilin@gmail.com", "Павел"));
+                    message.Subject = "Заголовок письма от " + DateTime.Now;
+                    message.Body = msg;
+                    //message.Attachments.Add(new Attachment());
+
+                    try
+                    {
+                        client.Send(message);
+                        MessageBox.Show("Почта успешно отправлена",
+                            "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    catch(Exception error)
+                    {
+                        MessageBox.Show(error.Message,
+                            "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
     }
 }
+                                                                                      
