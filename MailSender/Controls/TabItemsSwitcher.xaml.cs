6bc81995ded7114cec32d787cf5ecaec0791763f
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-21 19:10:12 +0300

    Пользовательский элемент управления

---
+++ MailSender/Controls/TabItemsSwitcher.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MailSender.Controls
+{
+    public partial class TabItemsSwitcher
+    {
+        public event EventHandler LeftButtonClick;
+        public event EventHandler RightButtonClick;
+
+        public bool LeftButtonVisible
+        {
+            get => LeftButton.Visibility == Visibility;
+            set => LeftButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        } 
+
+        public bool RightButtonVisible
+        {
+            get => RightButton.Visibility == Visibility;
+            set => RightButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public TabItemsSwitcher() => InitializeComponent();
+
+        private void ButtonBase_OnClick(object Sender, RoutedEventArgs E)
+        {
+            if(!(E.Source is Button button)) return;
+
+            switch (button.Name)
+            {
+                case "LeftButton":
+                    LeftButtonClick?.Invoke(this, EventArgs.Empty);
+                    break; 
+                case "RightButton":
+                    RightButtonClick?.Invoke(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}
--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,9 +1,41 @@
-﻿namespace MailSender
+﻿using System;
+using MailSender.Controls;
+
+namespace MailSender
 {
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
 
         private void ExitMenuItem_OnClick(object Sender, System.Windows.RoutedEventArgs E) => Close();
+
+        private void TabItemsSwitcher_OnLeftButtonClick(object Sender, EventArgs E)
+        {
+            if (!(Sender is TabItemsSwitcher switcher)) return;
+
+            if (MainTabCantrol.SelectedIndex == 0) return;
+
+            MainTabCantrol.SelectedIndex--;
+            if (MainTabCantrol.SelectedIndex == 0)
+            {
+                switcher.LeftButtonVisible = false;
+            }
+        }
+
+        private void TabItemsSwitcher_OnRightButtonClick(object Sender, EventArgs E)
+        {
+            if (!(Sender is TabItemsSwitcher switcher)) return;
+
+            var tab_count = MainTabCantrol.Items.Count;
+
+            if(MainTabCantrol.SelectedIndex == tab_count - 1) return;
+
+            MainTabCantrol.SelectedIndex++;
+
+            if (MainTabCantrol.SelectedIndex == MainTabCantrol.Items.Count - 1)
+            {
+                switcher.RightButtonVisible = false;
+            }
+        }
     }
 }
