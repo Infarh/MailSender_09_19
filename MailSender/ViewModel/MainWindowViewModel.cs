fdba3aae7ca3698dec1e8be7bc00abd17873b164
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-25 21:25:24 +0300

    Представление информации о получателях почты

---
+++ MailSender/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using MailSender.lib.Data.Linq2SQL;
+using MailSender.lib.Services;
+
+namespace MailSender.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private RecipientsDataProvider _RecipientsProvider;
+
+        private string _WindowTitle = "Рассыльщик почты v0.1";
+
+        public string WindowTitle
+        {
+            get => _WindowTitle;
+            set => Set(ref _WindowTitle, value);
+        }
+
+        public ObservableCollection<Recipient> Recipients { get; } = new ObservableCollection<Recipient>();
+
+        public MainWindowViewModel(RecipientsDataProvider RecipientsProvider)
+        {
+            _RecipientsProvider = RecipientsProvider;
+
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            var recipients = Recipients;
+            recipients.Clear();
+            foreach (var recipient in _RecipientsProvider.GetAll())
+                recipients.Add(recipient);
+        }
+    }
+}
---
+++ MailSender/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,41 @@
+using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using MailSender.lib.Data.Linq2SQL;
+using MailSender.lib.Services;
+
+namespace MailSender.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            var services = SimpleIoc.Default;
+            ServiceLocator.SetLocatorProvider(() => services);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            services.Register<MainWindowViewModel>();
+
+            services.Register<RecipientsDataProvider>();
+            services.Register(() => new MailSenderDBDataContext());
+
+        }
+
+        public MainWindowViewModel MainWindowModel => ServiceLocator.Current.GetInstance<MainWindowViewModel>();
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
