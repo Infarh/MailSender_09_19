c440b64507db5572e7779214aee8720254ab0298
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-28 17:40:04 +0300

    Интерфейсы основных поставщиков данных

--- MailSender.lib/Services/Interfaces/IRecipientsDataProvider.cs
+++ MailSender.lib/Services/Interfaces/IRecipientsDataProvider.cs
@@ -3,18 +3,5 @@ using MailSender.lib.Entityes;
 
 namespace MailSender.lib.Services.Interfaces
 {
-    public interface IRecipientsDataProvider
-    {
-        IEnumerable<Recipient> GetAll();
-
-        Recipient GetById(int id);
-
-        int Create(Recipient item);
-
-        void Edit(int id, Recipient item);
-
-        bool Remove(int id);
-
-        void SaveChanges();
-    }
+    public interface IRecipientsDataProvider : IDataProvider<Recipient> { }
 }
