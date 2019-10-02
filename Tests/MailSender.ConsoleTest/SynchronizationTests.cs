78a74f8dedce097b26a11154cf9f3612d8fdcd1c
Infarh <shmachilin@yandex.ru>
2019-10-02 21:19:49 +0300

    Пул потоков

--- Tests/MailSender.ConsoleTest/SynchronizationTests.cs
+++ Tests/MailSender.ConsoleTest/SynchronizationTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,22 +12,68 @@ namespace MailSender.ConsoleTest
 {
     internal static class SynchronizationTests
     {
-        private static List<string> __Messages = new List<string>();
+        private static readonly List<string> __Messages = new List<string>();
 
         public static void Start()
         {
-            var threads = new Thread[10];
+            //var threads = new Thread[10];
+
+            //for (var i = 0; i < threads.Length; i++)
+            //{
+            //    var i0 = i;
+            //    threads[i] = new Thread(() => Printer($"Message {i0}"));
+            //}
+
+            //Array.ForEach(threads, thread => thread.Start());
+
+            //Mutex mutex = new Mutex(true, "Имя мютекса");
+            ////mutex.WaitOne();
+            //mutex.ReleaseMutex();
+
+            //var semaphore = new Semaphore(0, 5, "Имя семафора");
+
+            //semaphore.WaitOne();
+
+            //// Критическая секция
+
+            //semaphore.Release();
+
+            //var manual_event = new ManualResetEvent(false);
+            var auto_event = new AutoResetEvent(false);
 
 
+            var test_threads = Enumerable
+               .Range(0, 5)
+               .Select(i => new Thread(() =>
+                {
+                    Console.WriteLine("Поток {0} ожидает запуска...", Thread.CurrentThread.ManagedThreadId);
+                    auto_event.WaitOne();
 
-            for (var i = 0; i < threads.Length; i++)
-            {
-                var i0 = i;
-                threads[i] = new Thread(() => Printer($"Message {i0}"));
-            }
+                    Console.WriteLine($"Поток {i}");
+                    Console.WriteLine("Поток {0} завершился", Thread.CurrentThread.ManagedThreadId);
 
-            Array.ForEach(threads, thread => thread.Start());
+                    auto_event.Reset();
+                })).ToArray();
+
+            foreach (var thread in test_threads)
+                thread.Start();
+
+            Console.WriteLine("Потоки ожидают запуска...");
+            Console.ReadLine();
+
+            auto_event.Set();
+
+            Console.ReadLine();
+
+            auto_event.Set();
+
+            Console.ReadLine();
+
+            auto_event.Set();
+
         }
+
+        private static readonly object __SyncRoot = new object();
 
         private static void Printer(string Message)
         {
@@ -32,15 +81,60 @@ namespace MailSender.ConsoleTest
 
             for (var i = 0; i < 20; i++)
             {
-                Console.Write("id:{0} ", Thread.CurrentThread.ManagedThreadId);
-                Console.Write(" - msg({0}):", i);
-                Console.WriteLine("\"{0}\"", Message);
+                lock (__SyncRoot)
+                {
+                    Console.Write("id:{0} ", Thread.CurrentThread.ManagedThreadId);
+                    Console.Write(" - msg({0}):", i);
+                    Console.WriteLine("\"{0}\"", Message);
+                    __Messages.Add(Message);
+                }
                 Thread.Sleep(100);
 
-                __Messages.Add(Message);
+                //try
+                //{
+                //    Monitor.Enter(__SyncRoot);
+
+                //    Console.Write("id:{0} ", Thread.CurrentThread.ManagedThreadId);
+                //    Console.Write(" - msg({0}):", i);
+                //    Console.WriteLine("\"{0}\"", Message);
+                //    __Messages.Add(Message);
+                //}
+                //finally
+                //{
+                //    if(Monitor.IsEntered(__SyncRoot))
+                //        Monitor.Exit(__SyncRoot);
+                //}
+                //Thread.Sleep(100);
+
             }
 
             Console.WriteLine("Поток {0} завершён", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+
+    [Synchronization]
+    internal class Logger : ContextBoundObject
+    {
+        private string _FilePath;
+
+        public string FilePath
+        {
+            get => _FilePath;
+            set
+            {
+                if (!File.Exists(value))
+                    throw new FileNotFoundException("Файл не существует", value);
+                _FilePath = value;
+            }
+        }
+
+        public Logger(string FilePath) => _FilePath = FilePath;
+
+        //[MethodImpl(MethodImplOptions.Synchronized)]
+        public void Log(string Message)
+        {
+            //lock(this)
+            File.AppendAllText(_FilePath, Message);
         }
     }
 }
