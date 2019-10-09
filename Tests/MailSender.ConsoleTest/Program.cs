935cf5ece014ef9bfc3aa271f90f45d032e5ac5a
Infarh <shmachilin@yandex.ru>
2019-10-09 20:43:41 +0300

    Простейшая работа с БД - загрузка/выгрузка данных. Простейшие запросы.

--- Tests/MailSender.ConsoleTest/Program.cs
+++ Tests/MailSender.ConsoleTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MailSender.ConsoleTest.Data;
 
 namespace MailSender.ConsoleTest
 {
@@ -10,6 +11,62 @@ namespace MailSender.ConsoleTest
     {
         static void Main(string[] args)
         {
+            using (var db = new SongsDB())
+            {
+                //db.Database.Log = msg => Console.WriteLine("EF: {0}\r\n-----------------", msg);
+
+                var tracks_count = db.Tracks.Count();
+                Console.WriteLine("В базе данных содержится {0} песен", tracks_count);
+
+                var long_tracks = db.Tracks.Where(track => track.Length > 500);
+
+                foreach (var info in long_tracks
+                   .Select(t => new
+                    {
+                        ArtistName = t.Artist.Name,
+                        t.Artist.Birthday
+                    }))
+                {
+                    Console.WriteLine("Artist:{0}, date:{1}", info.ArtistName, info.Birthday);
+                }
+
+                //db.Database.ExecuteSqlCommand()
+            }
+
+            //Console.WriteLine();
+            //Console.ReadLine();
+
+            //Console.Clear();
+
+            //using (var db = new SongsDB())
+            //{
+            //    db.Database.Log = msg => Console.WriteLine("EF: {0}\r\n-----------------", msg);
+
+            //    for (var i = 1; i <= 3; i++)
+            //    {
+            //        var artist = new Artist
+            //        {
+            //            Name = $"Artist name {i}",
+            //            Birthday = DateTime.Now.Subtract(TimeSpan.FromDays(365 * (i + 20))), 
+            //            Tracks = new List<Track>()
+            //        };
+
+            //        for (var j = 1; j < 3; j++)
+            //        {
+            //            var track = new Track
+            //            {
+            //                Name = $"Track {i + j}",
+            //                Length = j * 456
+            //            };
+            //            artist.Tracks.Add(track);
+            //        }
+
+            //        db.Artists.Add(artist);
+            //    }
+
+            //    db.SaveChanges();
+            //}
+
             Console.ReadLine();
         }
     }
