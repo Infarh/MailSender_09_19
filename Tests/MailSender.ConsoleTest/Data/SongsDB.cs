14eee52ea00e8380aaae379f2eb2fe5aa7830f5e
Infarh <shmachilin@yandex.ru>
2019-10-09 20:02:51 +0300

    Описание сущностей БД

--- Tests/MailSender.ConsoleTest/Data/SongsDB.cs
+++ Tests/MailSender.ConsoleTest/Data/SongsDB.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -9,16 +11,27 @@ namespace MailSender.ConsoleTest.Data
 {
     public class SongsDB : DbContext
     {
-
+        
     }
 
+    [Table("Track")]
     public class Track
     {
+        public int Id { get; set; }
 
+        [Required]
+        public string Name { get; set; }
+
+        public int Length { get; set; }
     }
 
     public class Artist
     {
+        public int Id { get; set; }
 
+        [Required, MinLength(3)]
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
     }
 }
